a95f401d9abfc68318b96223f6216dbb5705096c
gmacucella <g.acucella@studenti.uniba.it>
2024-06-25 12:10:05 +0200

    commenti

--- Europa/Pulizia/pre-processing.py
+++ Europa/Pulizia/pre-processing.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-# Function to describe dataframe
+# Funzione per descrivere il dataframe
 def describe(df):
     variables = []
     dtypes = []
@@ -17,23 +17,23 @@ def describe(df):
         missing.append(df[item].isna().sum())
 
     output = pd.DataFrame({
-        'variable': variables,
+        'variabile': variables,
         'dtype': dtypes,
-        'count': count,
-        'unique': unique,
-        'missing value': missing
+        'conteggio': count,
+        'unico': unique,
+        'valore mancante': missing
     })
 
     return output
 
-# Function to remove entities from dataframe
+# Funzione per rimuovere entità dal dataframe
 def remove_entities(df, entities_to_remove):
     return df[~df['Entity'].isin(entities_to_remove)]
 
-# Current directory
+# Directory corrente
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Define file paths for CSV files
+# Definisci i percorsi dei file CSV
 file1 = os.path.join(current_dir, '..', 'DbOriginali', '1- mental-illnesses-prevalence.csv')
 file2 = os.path.join(current_dir, '..', 'DbOriginali', '2- burden-disease-from-each-mental-illness(1).csv')
 file4 = os.path.join(current_dir, '..', 'DbOriginali', '4- adult-population-covered-in-primary-data-on-the-prevalence-of-mental-illnesses.csv')
@@ -41,7 +41,7 @@ file5 = os.path.join(current_dir, '..', 'DbOriginali', '5- anxiety-disorders-tre
 file7 = os.path.join(current_dir, '..', 'DbOriginali', '7- number-of-countries-with-primary-data-on-prevalence-of-mental-illnesses-in-the-global-burden-of-disease-study.csv')
 filesec = os.path.join(current_dir, '..', 'DbOriginali', 'SocialEconomicData.csv')
 
-# Dictionary of file paths
+# Dizionario dei percorsi dei file
 file_paths = {
     'file1': file1,
     'file2': file2,
@@ -51,10 +51,10 @@ file_paths = {
     'filesec': filesec
 }
 
-# Check if all files exist
+# Verifica se tutti i file esistono
 if all(os.path.exists(path) for path in file_paths.values()):
 
-    # Read CSV files
+    # Leggi i file CSV
     df1 = pd.read_csv(file_paths['file1'])
     df2 = pd.read_csv(file_paths['file2'])
     df4 = pd.read_csv(file_paths['file4'])
@@ -62,7 +62,7 @@ if all(os.path.exists(path) for path in file_paths.values()):
     df7 = pd.read_csv(file_paths['file7'])
     df_sec = pd.read_csv(file_paths['filesec'])
 
-    # Rename columns in df1 and df2
+    # Rinomina le colonne in df1 e df2
     columns_df1 = {
         'Schizophrenia disorders (share of population) - Sex: Both - Age: Age-standardized': 'Schizophrenia disorders',
         'Depressive disorders (share of population) - Sex: Both - Age: Age-standardized': 'Depressive disorders',
@@ -81,29 +81,29 @@ if all(os.path.exists(path) for path in file_paths.values()):
     df1.rename(columns=columns_df1, inplace=True)
     df2.rename(columns=columns_df2, inplace=True)
 
-    # Merge df1 and df2
+    # Unisci df1 e df2
     df_merge = pd.merge(df1, df2, on=['Entity', 'Code', 'Year'], how='inner')
 
-    # Entities to remove from merge
+    # Entità da rimuovere dall'unione
     entities_to_remove_merge = ['World', 'Africa (IHME GBD)', 'America (IHME GBD)', 'Asia (IHME GBD)', 'Europe (IHME GBD)', 'European Union (27)', 'Australia', 'Low-income countries', 'High-income countries', 'Lower-middle-income countries']
     df_merge = remove_entities(df_merge, entities_to_remove_merge)
 
-    # Output file path for merged data
+    # Percorso del file di output per i dati uniti
     output_file_path_merge = os.path.join(current_dir, '..', 'DbDefinitivi', 'DisturbiMentali-DalysNazioniDelMondo.csv')
     df_merge.to_csv(output_file_path_merge, index=False)
     print(f"Il file è stato salvato con successo in: {output_file_path_merge}")
 
-    # Remove entities from df4 and df5
+    # Rimuovi entità da df4 e df5
     entities_to_remove_4 = ['World']
     entities_to_remove_5 = ['Beijing/Shanghai, China', 'High-income countries', 'Lower-middle-income countries', 'Medellin, Colombia', 'Murcia, Spain', 'Sao Paulo, Brazil', 'Upper-middle-income countries']
     df4 = remove_entities(df4, entities_to_remove_4)
     df5 = remove_entities(df5, entities_to_remove_5)
 
-    # Drop 'Code' column from df4 and df7
+    # Elimina la colonna 'Code' da df4 e df7
     df4.drop(columns=['Code'], inplace=True)
     df7.drop(columns=['Code'], inplace=True)
 
-    # Output file paths for df4, df5, df7
+    # Percorsi dei file di output per df4, df5, df7
     output_file_path_4 = os.path.join(current_dir, '..', 'DbDefinitivi', '4- adult-population-covered-in-primary-data-on-the-prevalence-of-mental-illnesses.csv')
     output_file_path_5 = os.path.join(current_dir, '..', 'DbDefinitivi', '5- anxiety-disorders-treatment-gap.csv')
     output_file_path_7 = os.path.join(current_dir, '..', 'DbDefinitivi', '7- number-of-countries-with-primary-data-on-prevalence-of-mental-illnesses-in-the-global-burden-of-disease-study.csv')
@@ -115,8 +115,7 @@ if all(os.path.exists(path) for path in file_paths.values()):
     df7.to_csv(output_file_path_7, index=False)
     print(f"Il file è stato salvato con successo in: {output_file_path_7}")
 
-    #Social Economic Data
-
+    # Dati socio-economici
     df_sec.rename(columns={
         #'Country Name': 'Entity',
         'Country Code': 'Code'
